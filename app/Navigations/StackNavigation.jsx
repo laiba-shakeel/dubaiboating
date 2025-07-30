13eff7d293c75026848ed0ae323dcf2eaed39dcd
laiba-shakeel <aqsa.shaqil@gmail.com>
2025-07-31 01:48:42 +0500

    drawer implementation

--- app/Navigations/StackNavigation.jsx
+++ app/Navigations/StackNavigation.jsx
@@ -1,52 +1,91 @@
+// import React from 'react';
+// import { NavigationContainer, useNavigation } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import SplashScreen from '../Screens/Auth/SplashScreen';
+// import HomeScreen from '../Screens/Home/HomeScreen';
+// import Header from '../Components/Header';
+// import SignupScreen from '../Screens/Auth/Signup';
+// import LoginScreen from '../Screens/Auth/Login';
+
+// const Stack = createStackNavigator();
+// const Navigation = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="SplashScreen">
+//         <Stack.Screen
+//           name="SplashScreen"
+//           component={SplashScreen}
+//           options={{ headerShown: false }}
+//         />
+//         <Stack.Screen
+//           name="HomeScreen"
+//           component={HomeScreen}
+//           options={{
+//             headerShown: true,
+//             header: () => (
+//               <Header
+//                 showLogin={true}
+//                 showRegister={true}
+//                 backgroundColor="#FFFFFF"
+//               />
+//             ),
+//           }}
+//         />
+
+//         <Stack.Screen
+//           name="LoginScreen"
+//           component={LoginScreen}
+//           options={{ headerShown: false }}
+//         />
+//         <Stack.Screen
+//           name="SignupScreen"
+//           component={SignupScreen}
+//           options={{ headerShown: false }}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default Navigation;
+
+
+
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from '../Screens/Auth/SplashScreen';
 import HomeScreen from '../Screens/Home/HomeScreen';
-import Header from '../Components/Header';
 import SignupScreen from '../Screens/Auth/Signup';
 import LoginScreen from '../Screens/Auth/Login';
+import Header from '../Components/Header';
 
 const Stack = createStackNavigator();
 
-const Navigation = () => {
+const StackNavigation = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SplashScreen">
-        <Stack.Screen
-          name="SplashScreen"
-          component={SplashScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="HomeScreen"
-          component={HomeScreen}
-          options={{
-            headerShown: true,
-            header: () => (
-              <Header
-                showLogin={true}
-                showRegister={true}
-                backgroundColor="#FFFFFF" // White background
-                onTitlePress={() => alert('Logo pressed!')}
-              />
-            ),
-          }}
-        />
-         <Stack.Screen
-          name="LoginScreen"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen
-          name="SignupScreen"
-          component={SignupScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="SplashScreen">
+      <Stack.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          header: () => (
+            <Header
+              showLogin
+              showRegister
+              backgroundColor="#fff"
+            />
+          ),
+        }}
+      />
+      <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="SignupScreen" component={SignupScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
   );
 };
 
-export default Navigation;
-
+export default StackNavigation;
